5ab2be4bb1df6095698ed977bd71634c0f1296a5
Jimmy Herman <jamesher@andrew.cmu.edu>
2021-08-06 13:01:44 -0400

    updated baseurl for gh pages

--- docs/assets/search.js
+++ docs/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome to Jekyll!",'url':"/jekyll/update/2021/06/26/welcome-to-jekyll.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome to Jekyll!",'url':"/learn-to-race.org/learn-to-race-challenge/jekyll/update/2021/06/26/welcome-to-jekyll.html"}]};
